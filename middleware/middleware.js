d38174b6925391e727fef1dcb8274a9f1864f4ed
aniket-bacancy <aniket.amin@bacancytechnology.com>
2019-09-30 15:08:53 +0530

    changed the token flow

--- middleware/middleware.js
+++ middleware/middleware.js
@@ -3,18 +3,13 @@ const secret_key = "HAR_HAR_MAHADEV"
 // const Sequelize = require('sequelize');
 const User = require("../model/model.js")
 exports.verifyToken = async function(req, res, next) {
-    if (!req.headers.token) {
-        res.send({
-            msg : "token is not there"
-        })
-    } else {
+    
         var decodedPayload = jwt.verify(req.headers.token, secret_key)
         const user = await User.User.findAll({
             where : {
                 email : decodedPayload.email
             }
         })
-        console.log("user--->", user)
         if (user.length != 0){
             next()
         } else {
@@ -22,7 +17,6 @@ exports.verifyToken = async function(req, res, next) {
                 msg : "this user is not in database"
             })
         }
-    }
 }
 
 exports.checkRoute = function(req, res, next){
